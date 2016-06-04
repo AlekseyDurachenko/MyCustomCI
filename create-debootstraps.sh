759c24fdf3757784fe48be0e6ab23ec1bca2486a
Aleksey Durachenko <durachenko.aleksey@gmail.com>
2016-06-04 15:10:56 +0600

    debian squeeze has been archived

--- create-debootstraps.sh
+++ create-debootstraps.sh
@@ -8,8 +8,8 @@ debootstrap --variant=buildd --arch=amd64 trusty /home/chroots/ubuntu_trusty_amd
 debootstrap --variant=buildd --arch=i386 wily /home/chroots/ubuntu_wily_i386 http://archive.ubuntu.com/ubuntu/
 debootstrap --variant=buildd --arch=amd64 wily /home/chroots/ubuntu_wily_amd64 http://archive.ubuntu.com/ubuntu/
 # debian 6
-debootstrap --variant=buildd --arch=i386 squeeze /home/chroots/debian_squeeze_i386 http://http.debian.net/debian/
-debootstrap --variant=buildd --arch=amd64 squeeze /home/chroots/debian_squeeze_amd64 http://http.debian.net/debian/
+debootstrap --variant=buildd --arch=i386 squeeze /home/chroots/debian_squeeze_i386 http://archive.debian.net/debian/
+debootstrap --variant=buildd --arch=amd64 squeeze /home/chroots/debian_squeeze_amd64 http://archive.debian.net/debian/
 # debian 7
 debootstrap --variant=buildd --arch=i386 wheezy /home/chroots/debian_wheezy_i386 http://http.debian.net/debian/
 debootstrap --variant=buildd --arch=amd64 wheezy /home/chroots/debian_wheezy_amd64 http://http.debian.net/debian/
